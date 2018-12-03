dcf427d95b2bfffc18db2dd365baff2f019c4cdb
Allen <yuyuye958@qq.com>
2018-12-03 13:44:54 +0800

    初始化项目，添加 header 组件

---
+++ dist/precache-manifest.a0c2cfe70e781d4585a75b7a20d4075e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0fa8e005cfd97af967b6",
+    "url": "/js/chunk-vendors.3459f54d.js"
+  },
+  {
+    "revision": "9bd6b984ac271e72d312",
+    "url": "/js/app.d6f17b0e.js"
+  },
+  {
+    "revision": "78a132c1abc2e5719e47",
+    "url": "/js/about.826b3e79.js"
+  },
+  {
+    "revision": "7426b7c1b117d964979b8a004ff8437a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86f6a7c2f6cf9844d8a9f5d088d87406",
+    "url": "/img/cnodejs_light.86f6a7c2.svg"
+  },
+  {
+    "revision": "9bd6b984ac271e72d312",
+    "url": "/css/app.e9247943.css"
+  }
+];
---
+++ src/components/Header.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="header">
+    <a href="#">
+      <img src="../assets/cnodejs_light.svg" alt="headerBg">
+    </a>
+    <ul>
+      <li>
+        <a href="#">首页</a>
+      </li>
+      <li>
+        <a href="#">新手入门</a>
+      </li>
+      <li>
+        <a href="#">API</a>
+      </li>
+      <li>
+        <a href="#">关于</a>
+      </li>
+      <li>
+        <a href="#">注册</a>
+      </li>
+      <li>
+        <a href="#">登录</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style lang="scss" scoped>
+  .header {
+    background-color: #444444;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 50px;
+    > a {
+      margin-left: 6vw;
+      img {
+        width: 120px;
+      }
+    }
+    ul {
+      list-style: none;
+      display: flex;
+      align-items: center;
+      margin-right: 6vw;
+      li {
+        a {
+          text-decoration: none;
+          color: #ccc;
+          font-size: 13px;
+          padding: 10px 15px;
+          &:hover {
+            color: white;
+          }
+        }
+      }
+    }
+  }
+
+</style>
