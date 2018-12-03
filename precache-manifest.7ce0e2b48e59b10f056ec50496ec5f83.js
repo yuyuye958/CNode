2eb73d6fe476a3edd584a4b1f439ee91fca0f750
Allen <yuyuye958@qq.com>
2018-12-04 01:39:14 +0800

    deploy

---
+++ precache-manifest.7ce0e2b48e59b10f056ec50496ec5f83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "079d845f491b936657707ca66e4fe997",
+    "url": "reset.css"
+  },
+  {
+    "revision": "95d27e1a78c16a372c016d8209272319",
+    "url": "index.html"
+  },
+  {
+    "revision": "1e68a6735f0cab104c62",
+    "url": "assets/js/chunk-vendors.2750a6ce.js"
+  },
+  {
+    "revision": "e6f7245fd6148cb7e01c",
+    "url": "assets/js/app.1f907b2a.js"
+  },
+  {
+    "revision": "078fcd0940076c44bf21d40b8cf5d3b1",
+    "url": "assets/img/loading.078fcd09.gif"
+  },
+  {
+    "revision": "86f6a7c2f6cf9844d8a9f5d088d87406",
+    "url": "assets/img/cnodejs_light.86f6a7c2.svg"
+  },
+  {
+    "revision": "e6f7245fd6148cb7e01c",
+    "url": "assets/css/app.d350c128.css"
+  }
+];
